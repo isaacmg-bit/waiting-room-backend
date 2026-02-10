c159d6a13ce832c7bf88b9d4f709e09ad49e4db7
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-02-10 14:01:23 +0100

    chore: connected backend to mongodb, defined user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
