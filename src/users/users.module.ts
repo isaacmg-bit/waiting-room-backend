7ceb3b758863e57bf66828a084e8f0be512e40b1
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-22 17:01:32 +0100

    fix: forgot to register requestservice on users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { SupabaseService } from 'src/supabase/supabase.service';
+import { SupabasePerRequestService } from 'src/supabase/supabase-per-request-service';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService, SupabaseService],
+  providers: [UsersService, SupabaseService, SupabasePerRequestService],
   exports: [UsersService],
 })
 export class UsersModule {}
