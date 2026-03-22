71fd62b8977d7485843a8ef976e85a7b58a325dc
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-22 16:36:28 +0100

    fix: changes so authenticated users have to go through supabase with the jwt to do stuff on the website

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Scope, Inject } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import type { Request } from 'express';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { Database } from 'src/database.types';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class SupabaseService {
-  private readonly supabase: SupabaseClient<Database>;
+  private client: SupabaseClient<Database>;
 
-  constructor() {
-    this.supabase = createClient<Database>(
-      process.env.SUPABASE_URL as string,
-      process.env.SUPABASE_SERVICE_ROLE_KEY as string,
-    );
-  }
+  constructor(@Inject(REQUEST) private readonly request: Request) {}
 
   getClient(): SupabaseClient<Database> {
-    return this.supabase;
+    if (this.client) return this.client;
+
+    const authHeader = this.request.headers['authorization'];
+    const token = authHeader?.split(' ')[1];
+
+    this.client = createClient<Database>(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_SERVICE_ROLE_KEY!,
+      {
+        global: {
+          headers: token ? { Authorization: `Bearer ${token}` } : {},
+        },
+      },
+    );
+
+    return this.client;
   }
 }
