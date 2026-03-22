bdc58013bad0c4b5a4b5f112a3bb3af7be0b0272
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-22 18:28:29 +0100

    fix: trying older version of dto

--- src/user-instruments/dto/create-user-instrument.dto.ts
+++ src/user-instruments/dto/create-user-instrument.dto.ts
@@ -1,15 +1,4 @@
-import { IsUUID, IsString, IsIn } from 'class-validator';
-
-export const LEVELS = ['Beginner', 'Intermediate', 'Advanced'] as const;
-export type Level = (typeof LEVELS)[number];
-
 export class CreateUserInstrumentDto {
-  @IsUUID('4', { message: 'instrument_id must be a valid UUID' })
   instrument_id: string;
-
-  @IsString()
-  @IsIn(LEVELS, {
-    message: 'level must be one of: Beginner, Intermediate, Advanced',
-  })
-  level: Level;
+  level: string;
 }
