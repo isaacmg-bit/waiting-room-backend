db54b4d784f164480cc09ff1d4150a72b39c3d1e
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-22 18:46:19 +0100

    fix: instrument catalog

--- src/instruments/instruments.module.ts
+++ src/instruments/instruments.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { InstrumentsService } from './instruments.service';
 import { InstrumentsController } from './instruments.controller';
 import { SupabaseService } from 'src/supabase/supabase.service';
+import { SupabasePerRequestService } from 'src/supabase/supabase-per-request-service';
+import { SupabaseTokenGuard } from 'src/supabase/supabase-token-guard';
 
 @Module({
   controllers: [InstrumentsController],
-  providers: [InstrumentsService, SupabaseService],
+  providers: [
+    InstrumentsService,
+    SupabaseService,
+    SupabasePerRequestService,
+    SupabaseTokenGuard,
+  ],
 })
 export class InstrumentsModule {}
